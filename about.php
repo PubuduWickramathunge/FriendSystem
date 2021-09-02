4cbd11f50ce6e58a5772baa58f8ec9ad28de478b
Pubudu Wickramathunge <pubuduwickramathunge99@gmail.com>
2021-09-02 17:19:34 +0530

    about the system page

---
+++ about.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>About</title>
+<style>
+
+	body{
+		border: double;
+		padding: 50px;
+		text-align: center;
+			
+	}
+</style>
+</head>
+
+<body>
+	<h1>My Friend System</h1>
+	<h3>About</h3>
+	<p>
+		This is simple friends adding and removing system. You can signup and use the system to manage your friend list. This will show you number of friends, available users and your current friend list<br><br>
+		
+	</p>
+	<a href="signup.php"> Sign-Up </a> &ensp;
+	<a href="login.php"> Log-In </a> &ensp;
+	<a href="main.html"> Home Page </a> 
+	<h5>Thank you for using!</h5>
+</body>
+</html>
+
+</body>
+</html>
